a4e7610292d8b9c935cef71e9a72693fee7bcb9c
Kristina <kr.hranovska@gmail.com>
2024-07-17 22:05:27 +0300

    front catalog is ready

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -37,87 +37,87 @@ const CarItem = ({ data }) => {
     <div className={style.cardCar}>
       <img className={style.cardImg} src={data.gallery[0]} alt={data.name} />
 
-      <div className={style.carInfoThumb}>
-        <div>
-          <h2 className={style.carTitle}>{data.name}</h2>
-          <p className={style.carPrice}>&#8364;{data.price}.00</p>
-        </div>
-        <svg
-          className={`${style.iconHeart} ${isActive ? style.active : ""}`}
-          onClick={handleClick}
-        >
-          <use xlinkHref={`${sprite}#icon-favorite`} />
-        </svg>
-      </div>
-
-      <div className={style.carReviewsThumb}>
-        <div className={style.carThumb}>
-          <svg className={`${style.icon}`}>
-            <use xlinkHref={`${sprite}#icon-star`} />
+      <div>
+        <div className={style.carInfoThumb}>
+          <div className={style.carInfo}>
+            <h2 className={style.carTitle}>{data.name}</h2>
+            <p className={style.carPrice}>&#8364;{data.price}.00</p>
+          </div>
+          <svg
+            className={`${style.iconHeart} ${isActive ? style.active : ""}`}
+            onClick={handleClick}
+          >
+            <use xlinkHref={`${sprite}#icon-favorite`} />
           </svg>
-          <p className={style.carReviewsText}>
-            {data.rating}({data.reviews.length} Reviews)
-          </p>
         </div>
-        <div className={style.carThumb}>
-          <svg className={`${style.icon} ${style.fillStyle}`}>
-            <use xlinkHref={`${sprite}#icon-map`} />
-          </svg>
-          <p className={style.carLocationText}>{data.location}</p>
+
+        <div className={style.carReviewsThumb}>
+          <div className={style.carThumb}>
+            <svg className={`${style.icon}`}>
+              <use xlinkHref={`${sprite}#icon-star`} />
+            </svg>
+            <p className={style.carReviewsText}>
+              {data.rating}({data.reviews.length} Reviews)
+            </p>
+          </div>
+          <div className={style.carThumb}>
+            <svg className={`${style.icon} ${style.fillStyle}`}>
+              <use xlinkHref={`${sprite}#icon-map`} />
+            </svg>
+            <p className={style.carLocationText}>{data.location}</p>
+          </div>
         </div>
-      </div>
 
-      <p className={style.textDescription}>{data.description}</p>
+        <p className={style.textDescription}>{data.description}</p>
 
-      <ul className={style.categoriesList}>
-        <li>
-          <Categories
-            title={`${data.adults} adults`}
-            svg="users"
-            className={style.strokeStyle}
-          />
-        </li>
-        <li>
-          <Categories
-            title={`${capitalizeFirstLetter(data.transmission)}`}
-            svg="automatic"
-            className={style.fillStyle}
-          />
-        </li>
-        <li>
-          <Categories
-            title={`${capitalizeFirstLetter(data.engine)}`}
-            svg="petrol"
-            className={style.strokeStyle}
-          />
-        </li>
-
-        {data.details.kitchen > 0 && (
+        <ul className={style.categoriesList}>
           <li>
             <Categories
-              title={`Kitchen`}
-              svg="kitchen"
+              title={`${data.adults} adults`}
+              svg="users"
+              className={style.strokeStyle}
+            />
+          </li>
+          <li>
+            <Categories
+              title={`${capitalizeFirstLetter(data.transmission)}`}
+              svg="automatic"
               className={style.fillStyle}
             />
           </li>
-        )}
-        {data.details.beds > 0 && (
           <li>
             <Categories
-              title={`${data.details.beds} beds`}
-              svg="bed"
-              className={style.fillStyle}
+              title={`${capitalizeFirstLetter(data.engine)}`}
+              svg="petrol"
+              className={style.strokeStyle}
             />
           </li>
-        )}
-        {data.details.airConditioner > 0 && (
-          <li>
-            <Categories title={`AC`} svg="ac" className={style.strokeStyle} />
-          </li>
-        )}
-      </ul>
-
-      <MainButton title="Show more" btnType="main" className={style.carBtn} />
+          {data.details.kitchen > 0 && (
+            <li>
+              <Categories
+                title={`Kitchen`}
+                svg="kitchen"
+                className={style.fillStyle}
+              />
+            </li>
+          )}
+          {data.details.beds > 0 && (
+            <li>
+              <Categories
+                title={`${data.details.beds} beds`}
+                svg="bed"
+                className={style.fillStyle}
+              />
+            </li>
+          )}
+          {data.details.airConditioner > 0 && (
+            <li>
+              <Categories title={`AC`} svg="ac" className={style.strokeStyle} />
+            </li>
+          )}
+        </ul>
+        <MainButton title="Show more" btnType="main" className={style.carBtn} />
+      </div>
     </div>
   );
 };
