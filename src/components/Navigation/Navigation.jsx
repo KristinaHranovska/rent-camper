f0359665af9cdd32b4d2d4282bb8b88783dc9ca4
Kristina <kr.hranovska@gmail.com>
2024-07-18 10:57:29 +0300

    add logo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Hamburger from "shared/componets/Hamburger/Hamburger";
+import Menu from "components/Menu/Menu";
 
 import style from "./Navigation.module.css";
-import Menu from "components/Menu/Menu";
+import { default as logo } from "assets/images/logo.webp";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,6 +22,9 @@ const Navigation = () => {
 
   return (
     <nav className={style.navigation}>
+      <NavLink className={style.navLink} to="/">
+        <img className={style.logo} src={logo} alt="logo" />
+      </NavLink>
       <Hamburger active={menuOpen} toggleMenu={toggleMenu} />
       <Menu isOpen={menuOpen} toggleMenu={toggleMenu} />
     </nav>
