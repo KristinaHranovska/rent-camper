d930ea63a599913b60c0c17f85417cf88f351811
Kristina <kr.hranovska@gmail.com>
2024-07-18 11:04:32 +0300

    Update HomeVideo.jsx

--- src/components/HomeVideo/HomeVideo.jsx
+++ src/components/HomeVideo/HomeVideo.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useRef } from "react";
 import style from "./HomeVideo.module.css";
 import { default as video } from "shared/video/video.mp4";
 
 const HomeVideo = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, []);
+
   return (
     <div className={style.container}>
-      <video className={style.homeVideo} autoPlay muted loop>
+      <video
+        ref={videoRef}
+        className={style.homeVideo}
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
         <source type="video/mp4" src={video} />
       </video>
     </div>
